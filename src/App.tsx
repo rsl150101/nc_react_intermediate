3508c1712a457c1cc606f22078635285edb393df
rsl150101 <dnwnd501@naver.com>
2025-02-23 17:46:33 +0900

    [Add] 3.5 how define DOM event type

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import Circle from "./Circle";
+import React, { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const { value } = event.currentTarget;
+
+    setValue(value);
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
   return (
     <div>
-      <Circle bgColor={"teal"} />
-      <Circle bgColor={"tomato"} />
+      <form onSubmit={onSubmit}>
+        <input value={value} onChange={onChange} type="text" />
+        <button type="submit">Log in</button>
+      </form>
     </div>
   );
 }
