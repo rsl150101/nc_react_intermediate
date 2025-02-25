b4c3f350166f44da0298c1959273c5264052dfdd
rsl150101 <dnwnd501@naver.com>
2025-02-26 00:09:09 +0900

    [Add] 4.7 how to use useOutletContext hook

--- src/screens/users/Users.tsx
+++ src/screens/users/Users.tsx
@@ -1,12 +1,22 @@
-import { useParams } from "react-router-dom";
+import { useOutletContext, useParams } from "react-router-dom";
 import { users } from "../../db";
+
+interface IDarkMode {
+  darkMode: string;
+}
 
 function User() {
   const { userId } = useParams();
+  const { darkMode } = useOutletContext<IDarkMode>();
+  console.log(darkMode);
+
   return (
-    <h1>
-      User with it {userId} is named: {users[Number(userId) - 1].name}
-    </h1>
+    <>
+      <h1>
+        User with it {userId} is named: {users[Number(userId) - 1].name}
+      </h1>
+      <h3> DarkMode : {darkMode} </h3>
+    </>
   );
 }
 
