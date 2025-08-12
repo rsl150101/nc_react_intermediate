316382c3f4495d49823182b1fb6a7c4f3d281ae9
rsl150101 <dnwnd501@naver.com>
2025-08-12 22:13:07 +0900

    [Update] 6.10 todo list frame with RHF

---
+++ src/components/ToDoList.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+interface IForm {
+  toDo: string;
+}
+
+function ToDoList() {
+  const { register, handleSubmit, setValue } = useForm<IForm>();
+
+  const onValid = (data: IForm) => {
+    setValue("toDo", "");
+  };
+
+  return (
+    <div>
+      <h1>To Dos</h1>
+      <hr />
+      <form onSubmit={handleSubmit(onValid)}>
+        <input
+          {...register("toDo", {
+            required: "This field is required.",
+          })}
+          placeholder="Write a to do"
+        />
+        <button>Add</button>
+      </form>
+      <ul></ul>
+    </div>
+  );
+}
+
+export default ToDoList;
