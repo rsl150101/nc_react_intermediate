198d9487fcfe114a2834f4e1129f4c6c8162114d
rsl150101 <dnwnd501@naver.com>
2025-12-10 23:06:49 +0900

    [Add] 8.4 motion tutorial: variants orchestration

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { motion } from "motion/react";
+import { motion, stagger } from "motion/react";
 
 const WrapperDiv = styled.div`
   width: 100vw;
@@ -12,20 +12,59 @@ const WrapperDiv = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
-  border-radius: 10px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const myVars = {
-  start: { scale: 0 },
-  end: { scale: 1, rotateZ: 360, transition: { delay: 0.5 } },
+const Circle = styled(motion.div)`
+  width: 70px;
+  height: 70px;
+  background-color: white;
+  border-radius: 50%;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  place-self: center;
+`;
+
+const boxVariants = {
+  start: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  end: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: stagger(0.2, { startDelay: 0.5, from: "first" }),
+    },
+  },
+};
+
+const circleVariants = {
+  start: {
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+  },
 };
 
 function App() {
   return (
     <WrapperDiv>
-      <Box variants={myVars} initial={"start"} animate={"end"} />
+      <Box variants={boxVariants} initial="start" animate="end">
+        {Array(4)
+          .fill(0)
+          .map((_, index) => (
+            <Circle key={index} variants={circleVariants} />
+          ))}
+      </Box>
     </WrapperDiv>
   );
 }
