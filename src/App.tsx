313ce1b20ba905508a8eab5b5e195f24716bd12c
rsl150101 <dnwnd501@naver.com>
2025-12-11 22:40:36 +0900

    [Add] 8.5 motion tutorial: basic gesture usage
    hover, tap, drag feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { motion, stagger } from "motion/react";
+import { motion } from "motion/react";
 
 const WrapperDiv = styled.div`
   width: 100vw;
@@ -12,59 +12,21 @@ const WrapperDiv = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Circle = styled(motion.div)`
-  width: 70px;
-  height: 70px;
-  background-color: white;
-  border-radius: 50%;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  place-self: center;
-`;
-
 const boxVariants = {
-  start: {
-    opacity: 0,
-    scale: 0.5,
-  },
-  end: {
-    opacity: 1,
-    scale: 1,
-    transition: {
-      duration: 0.5,
-      bounce: 0.5,
-      delayChildren: stagger(0.2, { startDelay: 0.5, from: "first" }),
-    },
-  },
-};
-
-const circleVariants = {
-  start: {
-    opacity: 0,
-    y: 10,
-  },
-  end: {
-    opacity: 1,
-    y: 0,
-  },
+  hover: { scale: 1.5, rotateZ: 90 },
+  click: { scale: 1, borderRadius: "50%" },
+  drag: { backgroundColor: "rgb(46,204,113)", transition: { duration: 3 } },
 };
 
 function App() {
   return (
     <WrapperDiv>
-      <Box variants={boxVariants} initial="start" animate="end">
-        {Array(4)
-          .fill(0)
-          .map((_, index) => (
-            <Circle key={index} variants={circleVariants} />
-          ))}
-      </Box>
+      <Box variants={boxVariants} drag whileDrag="drag" whileHover="hover" whileTap="click" />
     </WrapperDiv>
   );
 }
