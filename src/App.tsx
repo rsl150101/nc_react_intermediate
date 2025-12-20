cd522efdc62e6082a21f36ac0c9ce3dcf886c39d
rsl150101 <dnwnd501@naver.com>
2025-12-20 18:48:49 +0900

    [Add] 8.12 motion tutorial: implement slider with AnimatePresence

--- src/App.tsx
+++ src/App.tsx
@@ -17,27 +17,43 @@ const Box = styled(motion.div)`
   border-radius: 40px;
   position: absolute;
   top: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 const boxVariants: Variants = {
-  initial: { opacity: 0, scale: 0 },
-  visible: { opacity: 1, scale: 1, rotateZ: 360 },
-  hidden: { opacity: 0, scale: 0, y: 50 },
+  invisible: { x: 500, opacity: 0, scale: 0 },
+  visible: { x: 0, opacity: 1, scale: 1, transition: { duration: 1 } },
+  hidden: { x: -500, opacity: 0, scale: 0, transition: { duration: 1 } },
 };
 
 function App() {
-  const [showing, setShowing] = useState(false);
+  const [visible, setVisible] = useState(1);
 
-  const toggleShowing = () => setShowing((prev) => !prev);
+  const nextSlider = () => setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+  const prevSlider = () => setVisible((prev) => (prev === 1 ? 1 : prev - 1));
 
   return (
     <WrapperDiv>
-      <button onClick={toggleShowing}>Click</button>
+      <button onClick={prevSlider}>prev</button>
+      <button onClick={nextSlider}>next</button>
       <AnimatePresence>
-        {showing ? (
-          <Box variants={boxVariants} initial="initial" animate="visible" exit="hidden" />
-        ) : null}
+        {Array.from({ length: 10 }, (_v, i) => i + 1).map((i) =>
+          i === visible ? (
+            <Box
+              variants={boxVariants}
+              initial="invisible"
+              animate={"visible"}
+              exit="hidden"
+              key={i}
+            >
+              {i}
+            </Box>
+          ) : null
+        )}
       </AnimatePresence>
     </WrapperDiv>
   );
