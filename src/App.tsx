fdc150c01957e24a4a304d7d28a7666d41545eee
rsl150101 <dnwnd501@naver.com>
2025-12-22 23:29:41 +0900

    [Add] 8.14 motion tutorial: basic layout animation usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,82 +1,46 @@
 import styled from "styled-components";
-import { AnimatePresence, motion, type Variants } from "motion/react";
+import { motion } from "motion/react";
 import { useState } from "react";
 
 const WrapperDiv = styled(motion.div)`
   width: 100vw;
   height: 100vh;
   display: flex;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
 `;
 
-const Box = styled(motion.div)`
+const Box = styled(motion.div)<{ $clicked: boolean }>`
   width: 400px;
   height: 400px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
-  position: absolute;
-  top: 100px;
   display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 28px;
+  justify-content: ${(props) => (props.$clicked ? "center" : "flex-start")};
+  align-items: ${(props) => (props.$clicked ? "center" : "flex-start")};
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants: Variants = {
-  entry: (isBack: boolean) => ({ x: isBack ? -500 : 500, opacity: 0, scale: 0 }),
-  center: { x: 0, opacity: 1, scale: 1, transition: { duration: 0.3 } },
-  exit: (isBack: boolean) => ({
-    x: isBack ? 500 : -500,
-    opacity: 0,
-    scale: 0,
-    transition: { duration: 0.3 },
-    rotateX: 90,
-  }),
-};
+const Circle = styled(motion.div)`
+  background-color: #00a5ff;
+  height: 100px;
+  width: 100px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  border-radius: 50%;
+`;
 
 function App() {
-  const [visible, setVisible] = useState(1);
-  const [isBack, setIsBack] = useState(false);
-  const [leaving, setLeaving] = useState(false);
+  const [clicked, setClicked] = useState(false);
 
-  const nextSlider = () => {
-    if (leaving) return;
-    setLeaving(true);
-    setIsBack(false);
-    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
-    if (visible === 10) setLeaving(false);
-  };
-
-  const prevSlider = () => {
-    if (leaving) return;
-    setLeaving(true);
-    setIsBack(true);
-    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
-    if (visible === 1) setLeaving(false);
-  };
-
-  const onAnimationEnd = () => {
-    setLeaving(false);
+  const handleClicked = () => {
+    setClicked((prev) => !prev);
   };
 
   return (
-    <WrapperDiv>
-      <button onClick={prevSlider}>prev</button>
-      <button onClick={nextSlider}>next</button>
-      <AnimatePresence custom={isBack} onExitComplete={onAnimationEnd}>
-        <Box
-          custom={isBack}
-          variants={boxVariants}
-          initial="entry"
-          animate="center"
-          exit="exit"
-          key={visible}
-        >
-          {visible}
-        </Box>
-      </AnimatePresence>
+    <WrapperDiv onClick={handleClicked}>
+      <Box $clicked={clicked}>
+        <Circle layout />
+      </Box>
     </WrapperDiv>
   );
 }
