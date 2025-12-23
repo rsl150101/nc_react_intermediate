e82bee516b662b323e4e45e39d6c3bc666d0a872
rsl150101 <dnwnd501@naver.com>
2025-12-24 01:44:33 +0900

    [Add] 8.15 motion tutorial: layout animation example

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,58 @@
 import styled from "styled-components";
-import { motion } from "motion/react";
+import { AnimatePresence, motion, type Variants } from "motion/react";
 import { useState } from "react";
 
 const WrapperDiv = styled(motion.div)`
   width: 100vw;
   height: 100vh;
   display: flex;
-  justify-content: space-around;
+  justify-content: center;
   align-items: center;
+`;
+
+const GridDiv = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 50vw;
+  gap: 10px;
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
 `;
 
 const Box = styled(motion.div)`
-  width: 400px;
-  height: 400px;
+  height: 300px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Circle = styled(motion.div)`
-  background-color: #00a5ff;
-  height: 100px;
-  width: 100px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  border-radius: 50%;
+const Overlay = styled(motion.div)`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
+
+const OverlayBox = styled(Box)`
+  width: 400px;
+  height: 400px;
+`;
+
+const overlayVariants: Variants = {
+  hidden: {
+    backgroundColor: "rgba(0,0,0,0)",
+  },
+  visible: {
+    backgroundColor: "rgba(0,0,0,0.5)",
+  },
+  exit: {
+    backgroundColor: "rgba(0,0,0,0)",
+  },
+};
 
 function App() {
   const [clicked, setClicked] = useState(false);
@@ -38,10 +63,19 @@ function App() {
 
   return (
     <WrapperDiv onClick={handleClicked}>
-      <Box>{!clicked ? <Circle layoutId="circle" style={{ borderRadius: "50%" }} /> : null}</Box>
-      <Box>
-        {clicked ? <Circle layoutId="circle" style={{ borderRadius: 0, scale: 1.5 }} /> : null}
-      </Box>
+      <GridDiv>
+        <Box layoutId="clicked" />
+        <Box />
+        <Box />
+        <Box />
+      </GridDiv>
+      <AnimatePresence>
+        {clicked ? (
+          <Overlay variants={overlayVariants} initial="hidden" animate="visible" exit="exit">
+            <OverlayBox layoutId="clicked" />
+          </Overlay>
+        ) : null}
+      </AnimatePresence>
     </WrapperDiv>
   );
 }
