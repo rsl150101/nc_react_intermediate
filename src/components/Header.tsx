562fe65a8fde8235206b13928caa8532ad31b69d
rsl150101 <dnwnd501@naver.com>
2025-12-25 02:26:37 +0900

    [Add] 9.1 navigation header component and router structure
    route: Home, Search, Tv

---
+++ src/components/Header.tsx
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: fixed;
+  width: 100%;
+  top: 0;
+  background-color: black;
+  font-size: 14px;
+  padding: 20px 60px;
+  color: white;
+`;
+
+const Col = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Logo = styled.svg`
+  margin-right: 50px;
+  width: 95px;
+  height: 25px;
+`;
+
+const Items = styled.ul`
+  display: flex;
+  align-items: center;
+`;
+
+const Item = styled.li`
+  margin-right: 20px;
+`;
+
+function Header() {
+  return (
+    <Nav>
+      <Col>
+        <Logo></Logo>
+        <Items>
+          <Item>Home</Item>
+          <Item>Tv Shows</Item>
+        </Items>
+      </Col>
+      <Col>
+        <button>Search</button>
+      </Col>
+    </Nav>
+  );
+}
+
+export default Header;
