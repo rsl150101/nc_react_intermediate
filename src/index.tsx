b7acd7e56a47704d08b43cd36644d8112d82f59e
rsl150101 <dnwnd501@naver.com>
2025-12-25 01:39:14 +0900

    [Initialize] 9.0 project settings

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import { createGlobalStyle } from "styled-components";
+import { Provider } from "react-redux";
 
 import App from "./App";
 import { theme } from "./theme";
+import { store } from "./store/configureStore";
 
 const GlobalStyle = createGlobalStyle`
 html, body, div, span, applet, object, iframe,
@@ -73,9 +75,11 @@ a {
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
